21a87141364eb57432b455272c813eda8d9552d4
Łukasz Bałabuch <lukas.balabuch@gmail.com>
2025-03-05 16:10:34 +0100

    potega-scheme

---
+++ 01/zadanie 8/Scheme/main.scm
@@ -0,0 +1,10 @@
+(define (potega x n)
+    (if (= n 0) 
+        1 
+        (if (= 0 (remainder n 2))
+            (* (potega x (/ n 2)) (potega x (/ n 2)))
+            (* x (potega x (- n 1)))
+            )
+        )
+    )
+(display (potega 2 12))
